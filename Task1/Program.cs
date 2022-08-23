cbd2830dea6cc39abc471a4a9a2707e9cf071a0e
Sarge <sarge2033@gmail.com>
2022-08-23 13:41:34 +0300

    задача 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -6,7 +6,7 @@ int[] array = new int[N];
     
     for (int i = 0; i < array.Length; i++)
     {
-        array[i] = new Random().Next(100, 999);
+        array[i] = new Random().Next(100, 1000);
         // Console.Write(array[i] + " ");
 
         if(array[i] % 2 == 0)
