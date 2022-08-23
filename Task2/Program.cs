2c41f2e2a188cb194499a3f0bbdacae6c6a1e871
Sarge <sarge2033@gmail.com>
2022-08-23 12:07:54 +0300

    задача 2

---
+++ Task2/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Write("Введите длину массива: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int Sum = 0; 
+int[] array = new int[N];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-99, 99);
+            if(i % 2 != 0)
+            {
+                Sum = Sum + array[i];
+            } 
+    }
+
+void WriteArray(int[] test)
+{
+    Console.Write("[");
+    
+        for(int i = 0; i < test.Length; i++)
+        {
+            if(i < test.Length -1)
+            {
+                Console.Write(test[i] + ", ");
+            }   
+            else
+            {
+                Console.Write(test[i] + "]");         
+            }
+        }
+        
+}
+
+WriteArray(array);
+Console.Write("\nСумма элементов, стоящих на нечётных позициях: " + Sum);
